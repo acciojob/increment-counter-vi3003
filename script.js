9728e602a920d0ba0199ab6ba969520e1ff9ed3c
Vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-03-22 20:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let counter = 0; // initialize counter variable
+
+		function incrementCounter() {
+			alert(counter);
+			counter++; // increment counter
+			document.getElementById("counter").textContent = counter; // update counter display
+		}
